a1cfe1f24393473d229683a107158f2808915244
ferdousalam007 <iamferdousalam007@gmail.com>
2021-08-06 10:52:21 +0600

    array sum  method

--- arraySum.js
+++ arraySum.js
@@ -5,4 +5,18 @@ for(let i =0; i<mynumber.length; i++){
    const element = mynumber[i];
    sum = sum + element;
 }
-console.log(sum);
+// console.log(sum);
+
+
+
+function arraysum(mynumber){
+    let sum = 0 ;
+    for(let i =0; i<mynumber.length; i++){
+        const element = mynumber[i];
+        sum = sum + element;
+     }
+     return sum;
+}
+const mynumber1 = [10, 12, 14, 74, 55, 87, 77,190];
+let result = arraysum(mynumber1);
+console.log(result);
